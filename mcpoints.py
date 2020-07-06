aedaedb7d4127e0ad666d820759aae7980ea50aa
poloz <polozfr@laposte.net>
2020-07-06 23:21:01 +0200

    Add a new class "point" which represents a point in mc The point has a constructor, which initialize the points components: x,y,z,dimension

---
+++ mcpoints.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*
+
+"""module to reprensent points in minecraft, and manage them in a list"""
+
+class Point:
+    """Class to represent a point in a minecraft world, represented by:
+        - x component
+        - y component
+        - z component
+        - dimension (overworld or nether or end)"""
+
+    def __init__(self, x, y, z, dimension):
+        """Initialize a point in a minecraft world
+        Parameters:
+            x: x component (integer)
+            y: y component (integer)
+            z: z component (integer)
+            dimension: string, possible values are:
+                - "overworld"
+                - "nether"
+                - "end"
+        """
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        self.x = x
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        self.y = y
+        if not isinstance(z, int):
+            raise TypeError("z must be an integer")
+        self.z = z
+        if dimension != "overworld"\
+           and dimension != "nether"\
+           and dimension != "end":
+            raise ValueError("unknown dimension \"{}\", sould be \"overworld\" or \"nether\" or \"end\"".format(dimension))
+        self.dimension = dimension
+
